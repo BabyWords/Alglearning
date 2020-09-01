ef74711371ccd380eeaaeafc8e4032a5f7b68ae5
PRINCETON\Jason_Wu <Jason_wu@epam.com>
2020-09-01 09:38:49 +0800

    refactor the code

--- src/main/java/leetcode/twoSum/Solution.java
+++ src/main/java/leetcode/twoSum/Solution.java
@@ -1,11 +1,13 @@
 package leetcode.twoSum;
 
+import com.sun.javafx.image.IntPixelGetter;
+
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
         }
