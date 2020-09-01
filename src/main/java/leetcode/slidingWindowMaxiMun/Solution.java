105d5ac00baa84fd0d98c6bdb7f4331d293fe7d4
Jason_wu <18061992960@163.com>
2020-09-01 22:16:49 +0800

    sliding window maximun

---
+++ src/main/java/leetcode/slidingWindowMaxiMun/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.slidingWindowMaxiMun;
+
+import java.util.LinkedList;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        int[] result= new int[nums.length-k+1];
+        for (int i = 0; i < nums.length; i++) {
+            while (!linkedList.isEmpty() && nums[linkedList.peekLast()] <= nums[i]) {
+                //全部弹出
+                linkedList.pollLast();
+            }
+            linkedList.addLast(i);
+            if (linkedList.peekFirst() <= i - k) {
+                linkedList.pollFirst();
+            }
+            if(i+1>=k){
+                result[i + 1 - k] = nums[linkedList.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
