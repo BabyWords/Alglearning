f971a4174e7f13dad4a583345f313c6e1767dec0
WuJunwen <18061992960@163.com>
2020-09-07 13:50:06 +0800

    Bulls and cows

---
+++ src/main/java/leetcode/nAryTreePreorderTraversal/Solution.java
@@ -0,0 +1,73 @@
+package leetcode.nAryTreePreorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+    /**
+     * 递归
+     * @param root
+     * @return
+     */
+    public List<Integer> preorder(Node root) {
+        List<Integer> integerList = new ArrayList<>();
+        helper(root, integerList);
+        return integerList;
+    }
+
+    public void helper(Node root, List<Integer> arrayList) {
+        if (root == null) {
+            return;
+        }
+        arrayList.add(root.val);
+        //关键就是吧每一个节点都需要去遍历他的 children
+        for (Node nodes : root.children) {
+            helper(nodes, arrayList);
+        }
+    }
+
+    /**
+     * 迭代
+     * 保证栈中的元素是当前这个 root,通过栈保存 root 然后通过 for 循环去遍历这个 root
+     * 外层在套一个 While 一个 root 一个 root 更新
+     * @param root
+     * @return
+     */
+    public List<Integer> preorder2(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            //头结点加入结果集
+            res.add(cur.val);
+            //将该节点的子节点从右往左压入栈
+            List<Node> nodeList = cur.children;
+            for (int i = nodeList.size() - 1; i >= 0; i--) {
+                stack.push(nodeList.get(i));
+            }
+        }
+        return res;
+    }
+
+
+}
---
+++ src/main/java/leetcode/validAnagram/Solution.java
@@ -0,0 +1,66 @@
+package leetcode.validAnagram;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    /**
+     * 暴力法
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> stringHashMap = new HashMap<>();
+        HashMap<Character, Integer> stringHashMap2 = new HashMap<>();
+        for (int i=0;i<s.length();i++){
+            stringHashMap.put(s.charAt(i), stringHashMap.getOrDefault(s.charAt(i), 0));
+            stringHashMap2.put(t.charAt(i), stringHashMap2.getOrDefault(t.charAt(i), 0));
+        }
+        return stringHashMap.equals(stringHashMap2);
+    }
+
+    /**
+     * Sort
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+
+    /**
+     * hash
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
