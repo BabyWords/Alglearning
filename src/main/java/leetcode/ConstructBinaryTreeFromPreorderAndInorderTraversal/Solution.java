915481088b5c5363331b69dc82d2f407a4ab912a
WuJunwen <18061992960@163.com>
2020-09-11 23:53:29 +0800

    leetCode 105 根据前序和中序输出二叉树

---
+++ src/main/java/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+import conmmon.TreeNode;
+
+class Solution {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end) {
+        // preorder 为空，直接返回 null
+        if (p_start == p_end) {
+            return null;
+        }
+        //先序遍历的第一个肯定是 root 节点
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+        //在中序遍历中找到根节点的位置
+        int i_root_index = 0;
+        for (int i = i_start; i < i_end; i++) {
+            if (root_val == inorder[i]) {
+                i_root_index = i;
+                break;
+            }
+        }
+        int leftNum = i_root_index - i_start;
+        //下面这段代码不需要想的太复杂,只是在把每一次传进来的 TreeNode 再根据中序和前序遍历,算出哪一部分是左边,哪一部分是右边,然后继续递归
+        //递归的构造左子树
+        root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index);
+        //递归的构造右子树
+        root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end);
+        return root;
+    }
+}
