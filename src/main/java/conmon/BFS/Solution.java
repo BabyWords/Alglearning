5a7a09bf9a414df1dfb3d492aae33514625f5b73
WuJunwen <18061992960@163.com>
2020-09-15 23:11:47 +0800

    DFS 和 BFS 的各种实现方式

--- src/main/java/conmon/BFS/Solution.java
+++ src/main/java/conmon/BFS/Solution.java
@@ -2,27 +2,78 @@ package conmon.BFS;
 
 import conmon.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 
 public class Solution {
+    public static void main(String[] args) {
+        TreeNode head=new TreeNode(1);
+        TreeNode second=new TreeNode(2);
+        TreeNode three=new TreeNode(3);
+        TreeNode four=new TreeNode(4);
+        TreeNode five=new TreeNode(5);
+        TreeNode six=new TreeNode(6);
+        TreeNode seven=new TreeNode(7);
+        head.right=three;
+        head.left=second;
+        second.right=five;
+        second.left=four;
+        three.right=seven;
+        three.left=six;
+        BroadFirstSearch(head);
+//        System.out.println(levelOrder(head));
+    }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    /**
+     * 直接输出
+     * @param nodeHead
+     */
+    //广度优先遍历是使用队列实现的
+    public static void BroadFirstSearch(TreeNode nodeHead) {
+        if (nodeHead == null) {
+            return;
+        }
+        Queue<TreeNode> myQueue = new LinkedList<>();
+        myQueue.add(nodeHead);
+        while (!myQueue.isEmpty()) {
+            TreeNode node = myQueue.poll();
+            System.out.print(node.val + " ");
+            if (null != node.left) {
+                myQueue.add(node.left);    //深度优先遍历，我们在这里采用每一行从左到右遍历
+            }
+            if (null != node.right) {
+                myQueue.add(node.right);
+            }
+
+        }
+    }
+
+    /**
+     * 按层次输出
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        //初始化一个结果集,最终的的结果为{{111},{222}}所以是 List 嵌套 List
         List<List<Integer>> allResults = new ArrayList<>();
+        //空值判断
         if (root == null) {
             return allResults;
         }
+        //利用队列的特性,存放每一层需要遍历的节点,广度遍历
         Queue<TreeNode> nodes = new LinkedList<>();
+        //初始化操作,下面才可以进行遍历
         nodes.add(root);
         while (!nodes.isEmpty()) {
+            //获取树的总大小
             int size = nodes.size();
+            //存放每一层的结果
             List<Integer> results = new ArrayList<>();
             for (int i = 0; i < size; i++) {
-                TreeNode node = nodes.poll();
+                //从队列中弹出就加入结果集
+                TreeNode node = nodes.poll();//重点.和上面那个写法不同的地方就在于是阿紫这个循环里面 poll 的
                 results.add(node.val);
+                //当前节点的左右节点加入队列,就是下一层
                 if (node.left != null) {
                     nodes.add(node.left);
                 }
--- src/main/java/conmon/DFS/Solution.java
+++ src/main/java/conmon/DFS/Solution.java
@@ -5,29 +5,59 @@ import conmon.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> allResults = new ArrayList<>();
-        if(root==null){
+        if (root == null) {
             return allResults;
         }
-        travel(root,0,allResults);
+        travel(root, 0, allResults);
         return allResults;
     }
 
-
-    private void travel(TreeNode root,int level,List<List<Integer>> results){
-        if(results.size()==level){
+    /**
+     * 按层次输出.递归写法
+     *  利用 DFS 达到 BFS 的效果,在 DFS 过程中记下 level,然后将结果按照 level 输出
+     * @param root
+     * @param level
+     * @param results
+     */
+    private void travel(TreeNode root, int level, List<List<Integer>> results) {
+        if (results.size() == level) {
             results.add(new ArrayList<>());
         }
         results.get(level).add(root.val);
-        if(root.left!=null){
-            travel(root.left,level+1,results);
+        if (root.left != null) {
+            travel(root.left, level + 1, results);
         }
-        if(root.right!=null){
-            travel(root.right,level+1,results);
+        if (root.right != null) {
+            travel(root.right, level + 1, results);
         }
     }
 
+    /**
+     * 直接输出,stack+循环
+     *
+     * @param nodeHead
+     */
+    //深度优先遍历
+    public void depthFirstSearch(TreeNode nodeHead) {
+        if (nodeHead == null) {
+            return;
+        }
+        Stack<TreeNode> myStack = new Stack<>();
+        myStack.add(nodeHead);
+        while (!myStack.isEmpty()) {
+            TreeNode node = myStack.pop();    //弹出栈顶元素
+            System.out.print(node.val + " ");
+            if (node.right != null) {
+                myStack.push(node.right);    //深度优先遍历，先遍历左边，后遍历右边,栈先进后出
+            }
+            if (node.left != null) {
+                myStack.push(node.left);
+            }
+        }
+    }
 }
