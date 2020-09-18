fb50e7587824911df2a98aa55695d5aa26ec6287
WuJunwen <18061992960@163.com>
2020-09-18 21:09:34 +0800

    跳跃游戏,贪心算法

---
+++ src/main/java/leetcode/JumpGame/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.JumpGame;
+
+/**
+ * LeetCode 55
+ */
+public class Solution {
+    //贪心算法,从后往前贪心
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
